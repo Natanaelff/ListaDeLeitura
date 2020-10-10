0e9319ada28b3133cbb7acb3c4958af7b65f5174
Natanaelff <natacfc111@gmail.com>
2020-10-10 02:22:59 -0300

    complemento da tela inicial

---
+++ src/pages/Main.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+
+const Main = () => {
+    const data = [
+        {
+            id: "1",
+            title: "Livro de teste",
+            anonations: "Muito bom!!!",
+            read: false,
+            photo: null,
+        },
+        {
+            id: "2",
+            title: "Livro de teste1",
+            anonations: "Exelente!",
+            read: false,
+            photo: null,
+        },
+        {
+            id: "3",
+            title: "Livro de teste2",
+            anonations: "Muito exemplar!!",
+            read: false,
+            photo: null,
+        },
+    ]
+
+    return (
+        <View style={styles.container}> 
+            <View style={styles.toolBox}>
+                <Text style={styles.title}>Lista de Leitura</Text>
+                <TouchableOpacity style={styles.add}>
+                    <Icon name="add" size={14} color={"#fff"} />
+                </TouchableOpacity>
+            </View>
+            <FlatList 
+                data={data} 
+                keyExtractor={item => item.id} 
+                renderItem={({item}) => (
+                    <TouchableOpacity style={styles.item}>
+                        <Text style={styles.itemText}>{item.title}</Text>
+                    </TouchableOpacity>
+                )} 
+            />
+            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 35,
+    }, 
+
+    toolBox: {
+        flexDirection: "row",
+        marginBottom: 5,
+    },
+    
+    title: {
+        fontSize: 16,
+        flex: 1,
+        color: "#16a085",
+    },
+
+    add: {
+        width: 22,
+        height: 22,
+        backgroundColor: "#16a085",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 50,
+    },
+
+    item: {
+
+    },
+
+    itemText: {
+        fontSize: 16,
+
+    },
+});
+
+export default Main;
